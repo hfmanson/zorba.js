453f4f3bcc689a0a634ebfd09bdbed0db8503394
Henri Manson <hfmanson@gmail.com>
2018-05-16 21:05:59 +0200

    cleanup

--- build_spiro.bat
+++ build_spiro.bat
@@ -1,1 +1,1 @@
-em++ -s WASM=1 -s EXPORTED_FUNCTIONS="['_run_query']" -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 -O2 src/spiro.cpp -Isystem/include/ -Lsystem/lib -lzorba_simplestore -o spiro.html
+em++ -s WASM=1 -s EXPORTED_FUNCTIONS="['_run_query']" -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 --shell-file shell_spiro.html -O2 src/spiro.cpp -Isystem/include/ -Lsystem/lib -lzorba_simplestore -o spiro.html
