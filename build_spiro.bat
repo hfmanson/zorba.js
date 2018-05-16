b1a223e1e2a0ebe9e5128c263b7d6fc509332b72
Henri Manson <hfmanson@gmail.com>
2018-05-16 17:22:54 +0200

    prepare for next commit

--- build_spiro.bat
+++ build_spiro.bat
@@ -1,2 +1,1 @@
 em++ -s WASM=1 -s EXPORTED_FUNCTIONS="['_run_query']" -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 -O2 src/spiro.cpp -Isystem/include/ -Lsystem/lib -lzorba_simplestore -o spiro.html
-REM em++ -s EXPORTED_FUNCTIONS="['_run_query']" -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 -O2 src/spiro.cpp -Isystem/include/ -Lsystem/lib -lzorba_simplestore -o spiro.html
